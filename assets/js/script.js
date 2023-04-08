e76b39973eba66d3e43d935632818cc47c485789
imransal <imrannasirsaleem@gmail.com>
2023-04-08 14:09:36 +0000

    Added Division game and ensured larger number is evenly divisible by the smaller number

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,6 +44,8 @@ function runGame(gameType) {
         displayMultiplyQuestion(num1, num2);
     } else if (gameType === "subtract") {
         displaySubtractQuestion (num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion (num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -89,6 +91,8 @@ function calculateCorrectAnswer() {
         return [operand1 * operand2, "multiply"];
     } else if (operator === "-") {
         return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [Math.floor(operand1 / operand2), "division"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -137,5 +141,22 @@ function displayMultiplyQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
     document.getElementById('operator').textContent = "x";
+
+}
+
+function displayDivisionQuestion(operand1, operand2) {
+
+    let largerOperand = operand1 > operand2 ? operand1 : operand2;
+    let smallerOperand = operand1 > operand2 ? operand2 : operand1;
+
+    // Ensure that the larger number is evenly divisible by the smaller number
+    let remainder = largerOperand % smallerOperand;
+    if (remainder !== 0) {
+        largerOperand += smallerOperand - remainder;
+    }
+
+    document.getElementById('operand1').textContent = largerOperand;
+    document.getElementById('operand2').textContent = smallerOperand;
+    document.getElementById('operator').textContent = "/";
 
 }
